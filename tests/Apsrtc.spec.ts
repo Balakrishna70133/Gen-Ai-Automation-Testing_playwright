22844a60b8fa54e1557b9e96fade4d6f2612ab35
Balakrishna70133 <bcbc7780@gmail.com>
2026-06-18 16:03:07 +0530

    Update Apsrtc.spec.ts

--- tests/Apsrtc.spec.ts
+++ tests/Apsrtc.spec.ts
@@ -38,12 +38,13 @@ test('Verify APSRTC website',async({page})=>{
     // await page.locator("input[id='txtJourneyDate']").fill("30/05/2026");
     // await page.keyboard.press("Escape");
     await page.click("input[id='txtJourneyDate']");
-    await page.locator("(//a[text()='15'])[1]").click();
+    await page.locator("(//a[text()='19'])[1]").click();
     await page.waitForTimeout(5000);
     await page.click("input[id='searchBtn']");
     await expect(page.getByText("Book ticket in advance")).toBeVisible();
     await page.locator("//span[text()='Bus Type']").click();
     await page.locator("//span[normalize-space()='A/C CLASS']").click();
+    await page.waitForTimeout(5000);
 
     let acServices = await page.locator("div.srvceNO:visible").all();
 
